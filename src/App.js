8af50f9c2de8ea93fe12a08ee0a5f2e5539b8a42
shootingstart <shootingstart42@gmail.com>
2020-12-11 04:44:49 -0500

    Add languages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import 'react-dropdown/style.css';
 import axios from 'axios';
+import Dropdown from 'react-dropdown';
 import "./App.css";
 
-const Convert = ({ text }) => {
+const Convert = ({ language, text }) => {
   const [convertedText, setConvertedText] = useState('');
 
   useEffect(() => {
-    const response = axios
-      .post(
+
+    const response =
+        language !== "en" &&
+        axios.post(
         'https://translation.googleapis.com/language/translate/v2',
         {},
         {
           params: {
             q: text,
             key: 'AIzaSyDbtoK1kfDx3BZxJDkMfHDh-vXvWzwdhKo',
-            source: "fr",
+            source: language,
             target: "en",  
           }
         }
@@ -25,27 +29,48 @@ const Convert = ({ text }) => {
         setConvertedText(response.data.data.translations[0].translatedText);
       })
       .catch((err) => {
-        console.log('rest api error', err);
+        console.log(language);
       });
   }, [text]);
 
   return <div>{convertedText}</div>;
 };
 
+const recogLanguageLanguage = [
+    {value: "en-US", label: "English"},
+    {value: "fr-FR", label:"French"},
+    {value: "zh-CN", label: "Mandarin Chinese"},
+    {value: "zh-HK", label: "Cantonese"}
+]
+
+
+
 const App = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition()
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [language, setLanguage] = useState("en-US");
+
+  useEffect(() => {
+      if (navigator && navigator.language) {
+          setLanguage(navigator.language);
+      }
+  }, []);
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null
   }
+    const startListen = (lang) => {
+        SpeechRecognition.startListening({language: lang});
+    }
 
   return (
     <div>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
+        <h4>Select your language</h4>
+        <Dropdown options={recogLanguageLanguage} onChange={(option) => setLanguage(option.value)} value={language}/>
+      <button onClick={startListen}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
       <p>{transcript}</p>
-      <Convert text={transcript}/>
+      <Convert language={language.split("-")[0]} text={transcript}/>
     </div>
   )
 }
